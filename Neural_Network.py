818fd1c65192cc88a39ad33d03afd35879099cb8
Barcelona16 <wqw16@mails.tsinghua.edu.cn>
2021-04-21 11:54:27 +0800

    torch.tensor

---
+++ Neural_Network.py
@@ -0,0 +1,10 @@
+'''
+Author: Deavan
+Date: 2021-04-19 14:42:59
+Description: 
+'''
+import torch
+from torch.autograd import Variable
+import torch.nn
+import torch.nn.functional as F
+
--- U-Net.py
+++ U-Net.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 x = torch.Tensor(5,3)
 # print(x)
@@ -17,12 +18,10 @@ x = torch.Tensor(5,3)
 
 from torch.autograd import Variable
 x = Variable(torch.ones(2,2), requires_grad = True)
-print(x)
 
 y = x
-print(y)
 
-print(y.grad_fn)
+# print(y.grad_fn)
 
 z = y * y * 3
 #out = z.mean()
@@ -38,9 +37,40 @@ x = torch.randn(3)
 x = Variable(x, requires_grad = True)
 
 y = x * 2
-print(y)
-print(y.data.norm())
+#print(y.data.norm()) #范数
 while y.data.norm() < 1000:
     y = y * 2
+y.backward(torch.ones_like(x))
+# print(x.grad)
 
-print(y)
+# print(x[...,2])
+
+x = torch.rand(3,3)
+y = torch.rand_like(x)
+z = torch.rand_like(x)
+#print(x,y,z)
+t1 = torch.cat([x,y,z], dim = 1)
+print(t1.shape)
+print(t1)
+
+y1 = t1 @ t1.T  #矩阵乘法
+y2 = t1.matmul(t1.T)  
+y3 = torch.rand_like(t1)
+torch.matmul(t1, t1.T, out = y3)
+print("- - - --  - - - - -- - - - -- -\n")
+print(y1, y2, y3)
+
+print(t1 * t1) # 点乘
+
+agg = t1.sum()
+print(agg) # tensor
+print(agg.item()) # value
+
+# tensor 2 numpy
+n1 = t1.numpy()
+print(n1.shape)
+
+# numpy 2 tensor
+n2 = np.ones(2)
+t2 = torch.from_numpy(n2)
+print(t2)
