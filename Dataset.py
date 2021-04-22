fad87c0a47c9e5cde9b394e566b3379572bdf5c3
Barcelona16 <wqw16@mails.tsinghua.edu.cn>
2021-04-22 15:02:30 +0800

    NN

--- Dataset.py
+++ Dataset.py
@@ -34,13 +34,50 @@ labels_map = {
     8: "Bag",
     9: "Ankle Boot",
 }
-figure = plt.figure(figsize=(8, 8))
-cols, rows = 3, 3
-for i in range(1, cols * rows + 1):
-    sample_idx = torch.randint(len(training_data), size=(1,)).item()
-    img, label = training_data[sample_idx]
-    figure.add_subplot(rows, cols, i)
-    plt.title(labels_map[label])
-    plt.axis("off")
-    plt.imshow(img.squeeze(), cmap="gray")
-plt.show()
+# figure = plt.figure(figsize=(8, 8))
+# cols, rows = 3, 3
+# for i in range(1, cols * rows + 1):
+#     sample_idx = torch.randint(len(training_data), size=(1,)).item()
+#     img, label = training_data[sample_idx]
+#     #print(training_data[sample_idx])
+#     figure.add_subplot(rows, cols, i)
+#     plt.title(labels_map[label])
+#     plt.axis("off")
+#     plt.imshow(img.squeeze(), cmap="gray")
+# plt.show()
+# print(training_data)
+
+import os
+import pandas as pd
+from torchvision.io import read_image
+from torch.utils.data import Dataset
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        sample = {"image": image, "label": label}
+        return sample
+
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+print(len(training_data))
+print(len(train_dataloader))
+print(len(test_dataloader))
--- Neural_Network.py
+++ Neural_Network.py
@@ -3,8 +3,46 @@ Author: Deavan
 Date: 2021-04-19 14:42:59
 Description: 
 '''
+import os
 import torch
-from torch.autograd import Variable
-import torch.nn
-import torch.nn.functional as F
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Using {} device'.format(device))
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.Linear1 = nn.Linear(28*28, 512)
+        self.relu1 = nn.ReLU()
+        self.Linear2 = nn.Linear(512, 512)
+        #self.relu2 = nn.ReLU()
+        self.Linear3 = nn.Linear(512, 10)
+        #self.relu3 = nn.ReLU()
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.Linear1(x)
+        #x = nn.ReLU(x)
+        x = self.relu1(x)
+        x = self.Linear2(x)
+        #x = self.relu1(x)
+        x = self.Linear3(x)
+        #x = self.relu1(x)
+        return x
+
+model = NeuralNetwork().to(device)
+print(model)
+
+X = torch.ones(1, 28, 28, device=device)
+print(X)
+logits = model(X)
+print(model.Linear1)
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
+
 
