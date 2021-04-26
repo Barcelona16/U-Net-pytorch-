c6a3017872dc160f6afb5915e221e72daa93181d
Barcelona16 <wqw16@mails.tsinghua.edu.cn>
2021-04-26 10:35:51 +0800

    add one layer

--- Neural_Network.py
+++ Neural_Network.py
@@ -8,9 +8,14 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from torchvision.transforms import ToTensor, Lambda
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using {} device'.format(device))
+
+learning_rate = 1e-3
+batch_size = 64
+epochs = 5
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -19,9 +24,9 @@ class NeuralNetwork(nn.Module):
         self.Linear1 = nn.Linear(28*28, 512)
         self.relu1 = nn.ReLU()
         self.Linear2 = nn.Linear(512, 512)
-        #self.relu2 = nn.ReLU()
+        self.relu2 = nn.ReLU()
         self.Linear3 = nn.Linear(512, 10)
-        #self.relu3 = nn.ReLU()
+        self.relu3 = nn.ReLU()
 
     def forward(self, x):
         x = self.flatten(x)
@@ -29,22 +34,79 @@ class NeuralNetwork(nn.Module):
         #x = nn.ReLU(x)
         x = self.relu1(x)
         x = self.Linear2(x)
-        #x = self.relu1(x)
+        x = self.relu2(x)
         x = self.Linear3(x)
-        #x = self.relu1(x)
+        x = self.relu3(x)
         return x
 
 model = NeuralNetwork().to(device)
 print(model)
 
-X = torch.ones(1, 28, 28, device=device)
-print(X)
-logits = model(X)
-print(model.Linear1)
-pred_probab = nn.Softmax(dim=1)(logits)
-y_pred = pred_probab.argmax(1)
-print(f"Predicted class: {y_pred}")
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
 
-for name, param in model.named_parameters():
-    print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+train_dataloader = DataLoader(training_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
+
+
+# X = torch.ones(1, 28, 28, device=device)
+# print(X)
+# logits = model(X)
+# print(model.Linear1)
+# pred_probab = nn.Softmax(dim=1)(logits)
+# y_pred = pred_probab.argmax(1)
+# print(f"Predicted class: {y_pred}")
+
+# for name, param in model.named_parameters():
+#     print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
+
+loss_fn = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+print(model.parameters)
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            #print("correct is   ",correct)
+    test_loss /= size
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
 
